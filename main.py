1dc68656bc3359a17a4f9fe018ba55a80c5ec7a9
ed13santi <37627980+ed13santi@users.noreply.github.com>
2022-07-01 16:28:45 +0100

    first finish of main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import random
+import matplotlib.pyplot as plt
 
 
 
@@ -11,12 +12,12 @@ class Node:
     def __init__(self, capacities):
         self.capacities = capacities # resource capacities for each resource type in the node
         self.remaining_capacities = self.capacities
+        self.processing_tasks = []
 
     def process_new_task(self, task):
         # check that task being added doesn't exceed available resources
         assert task.w > 0
-        for i in range(len(self.capacities)):
-            assert all(rc >= d for rc, d in zip(self.remaining_capacities, task.ds))
+        assert all(rc >= d for rc, d in zip(self.remaining_capacities, task.ds))
 
         self.processing_tasks.append(task)
         self.remaining_capacities = [rc - d for rc, d in zip(self.remaining_capacities, task.ds)]
@@ -39,12 +40,30 @@ class Task:
                
 
 class Cluster:
-    def __init__(self, nodes):
+    def __init__(self, nodes, network_adjacency_matrix_row, receive_external_tasks_bool, external_task_means=()):
         self.nodes = nodes
         self.received_tasks = []
         self.received_tasks_external = []
-        self.receive_external_tasks = False
-        self.external_task_means = ()
+        self.tasks_to_be_received = []
+        if receive_external_tasks_bool:
+            self.external_task_means = external_task_means
+        else:
+            self.external_task_means = ()
+        self.neighbors = self.get_neighbors(network_adjacency_matrix_row)
+
+    def local_utility(self):
+        tot = 0
+        for node in self.nodes:
+            for task in node.processing_tasks:
+                tot += task.u
+        return tot
+
+    def get_neighbors(self, network_adjacency_matrix_row):
+        neighbors = []
+        for i in range(network_adjacency_matrix_row.shape[0]):
+            if network_adjacency_matrix_row[i] != 0:
+                neighbors.append((i,network_adjacency_matrix_row[i]))
+        return neighbors
 
     def initialize_nodes_uniform_distribution(self, n_nodes, lows, highs):
         nodes = []
@@ -56,43 +75,68 @@ class Cluster:
             nodes.append(capacities)
         self.nodes = nodes
 
-    def receive_external_tasks(self, task_types):
-        new_tasks = []
+    def receive_external_tasks_func(self, task_types):
         for mean, task_type in zip(self.external_task_means, task_types):
             no_tasks_of_current_type = np.random.poisson(mean)
             for _ in range(no_tasks_of_current_type):
-                new_tasks.append(Task(task_type))
-
-        self.received_tasks_external = new_tasks 
-
-    def receive_all_tasks(self, task_types):
-        self.receive_external_tasks(task_types)
-        self.received_tasks = self.tasks_to_be_received + self.received_tasks_external
+                self.received_tasks.append(Task(task_type))
 
     def greedy_allocation(self):
         # sort received tasks in ascending order of utility
         self.received_tasks.sort(reverse=False, key=(lambda task: task.u))
-        # sort nodes in descending order of node capacity
+        # sort nodes in descending order of node capacity (first CPU and then network)
         self.nodes = self.nodes(reverse=True, key=(lambda node: node.capacities))
         # iterate through tasks in reverse order so that if you delete current element
         # it does not affect the index of the next element in the iteration
         for i in range(len(self.received_tasks)-1, -1, -1):
-            node_index = -1
             for j in range(len.self.nodes):
-
-            # need to reorder the nodes
-
+                enough_capacity = True
+                for c, d in zip(self.nodes[j].capacities, self.received_tasks[i].ds):
+                    if d > c:
+                        enough_capacity = False
+                if enough_capacity:
+                    self.nodes[j].process_new_task(self.received_tasks[i])
+                    del self.received_tasks[i]
+                    self.nodes = self.nodes(reverse=True, key=(lambda node: node.capacities))
+                    break
 
     def selectAndAllocate(self):
         self.greedy_allocation()
 
-    def iterate(self, task_types):
-        self.receive_all_tasks(task_types)
+    def random_forwarding(self):
+        out = []
+        for task in self.received_tasks:
+            selected_neighbor = random.choice(self.neighbors)
+            out.append((task, selected_neighbor))
+        self.received_tasks = []
+        return out
+
+    def forwardUnallocatedTasks(self):
+        return self.random_forwarding()
+
+    def advanceTime(self, task_types):
+        # reduce transfer time by 1 for tasks being transferred and when it reaches 0 add the tasks to received_tasks
+        for i in range(len(self.tasks_to_be_received)-1, -1, -1):
+            task, transfer_time = self.tasks_to_be_received[i]
+            transfer_time -= 1
+            if transfer_time == 0:
+                self.received_tasks.append(task)
+                del self.tasks_to_be_received[i]
+
+        # add externally received tasks to self.received_tasks
+        self.receive_external_tasks_func(task_types)
+
+        # reduce processing time by 1 for processing tasks and remove once it reaches 0
+        for node in self.nodes:
+            for i in range(len(node.processing_tasks)-1, -1, -1):
+                task = node.processing_tasks[i]
+                task.s -= 1
+                if task.s == 0:
+                    del node.processing_tasks[i]
+
+    def allocationAndRouting(self):
         self.selectAndAllocate()
-        # ALLOCATED <-- selectAndAllocate(TASKS)
-        # TASKS <-- TASKS \ ALLOCATED
-        # forward tasks remaining in TASKS
-        # update status of currently processing tasks
+        return self.forwardUnallocatedTasks()
 
 
 ##################################################################################################
@@ -109,22 +153,46 @@ def add_connection(matrix, a, b):
 ##################################################################################################
 
 def main():
+    no_clusters = 16
+
+    # matrix of transfer times between clusters (if 0 it means no connection so actually infinity in terms of time)
+    A = np.zeros((no_clusters, no_clusters))
+    for i in range(no_clusters-1):
+        add_connection(A,  i,  i+1)
+    add_connection(A, no_clusters-1,  0)
+    add_connection(A,  3,  12)
+    add_connection(A,  4,  11)
+
+    # task receivers
+    C_receivers = [5,6,9,10]
+
+    # received tasks means (tasks follow Poisson distribution)
+    received_tasks_means_heavy = [(40, 4,20, 8),
+                                  (28, 4,16, 4),
+                                  (32,16, 4, 8),
+                                  (28,24, 8, 6)]
+
+    received_tasks_means_light = [tuple(el/2 for el in listEl) for listEl in received_tasks_means_heavy]
+
+    # light or heavy task load
+    heavy = True # if false we use light
+
+    if heavy:
+        received_tasks_means = received_tasks_means_heavy
+    else:        
+        received_tasks_means = received_tasks_means_light
 
     # cluster is the list of clusters of respip ources
-    clusters = [Cluster([]) for _ in range(16)]
+    clusters = []
+    for i in range(no_clusters):
+        if i in C_receivers:
+            Cluster([], A[i], True, received_tasks_means[C_receivers.index(i)]) 
+        else:
+            Cluster([], A[i], False) 
     n_nodes_each_cluster = [32, 72, 52, 84, 64, 76, 60, 64, 76, 60, 80, 44, 64, 88, 56, 52]
     for c, n in zip(clusters, n_nodes_each_cluster):
         c.initialize_nodes_uniform_distribution(n, [50,50], [150,150]) # set nodes to have a random capacity for both CPU and netowrk in range [50,150]
         ### !EXCLUDES HIGH BUT IT DOESN'T MATTER IF VALUES TAKE CONTINUOUS DISTRIBUTION
-
-
-    # matrix of transfer times between clusters
-    A = np.zeros((len(clusters), len(clusters)))
-    for i in range(15):
-        add_connection(A,  i,  i+1)
-    add_connection(A, 15,  0)
-    add_connection(A,  3,  12)
-    add_connection(A,  4,  11)
 
 
     # task types
@@ -138,47 +206,33 @@ def main():
     R = ["CPU", "Network"]
 
 
-    # task receivers
-    C_receivers = [5,6,9,10]
-
-
-
-    # received tasks means (tasks follow Poisson distribution)
-    received_tasks_means_heavy = [(40, 4,20, 8),
-                                  (28, 4,16, 4),
-                                  (32,16, 4, 8),
-                                  (28,24, 8, 6)]
-
-    received_tasks_means_light = [tuple(el/2 for el in listEl) for listEl in received_tasks_means_heavy]
-
-
-
-    # light or heavy task load
-    heavy = True # if false we use light
-
-    if heavy:
-        received_tasks_means = received_tasks_means_heavy
-    else:        
-        received_tasks_means = received_tasks_means_light
-
-    for i, c in enumerate(clusters):
-        if i in C_receivers:
-            c.receive_external_tasks = True
-            c.external_task_means = received_tasks_means[C_receivers.index(i)]
-
-
     # communication limit
     communication_limit = 300
 
 
 
-    for time_step in range(2000):
-        # receive tasks
+    utility_rates = []
+    proc_tasks = []
+
+    for _ in range(2000):
         for c in clusters:
-            c.iterate()
+            c.advanceTime(task_types)
+        for c in clusters:
+            forwarded_tasks = c.allocationAndRouting()
+            for (task, (cluster_index, transfer_time)) in forwarded_tasks:
+                clusters[cluster_index].tasks_to_be_received.append((task, transfer_time))
+
+        cluster_utility_rates = [c.local_utility() for c in clusters]
+        utility_rate = sum(cluster_utility_rates)
+        utility_rates.append(utility_rate)
+        proc_tasks.append(sum([len(c.tasks_to_be_received) for c in clusters]))
+
+    plt.plot(proc_tasks)
+    plt.show()
+            
 
         
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
