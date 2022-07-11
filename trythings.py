ea0217b99c50f51ecff82bb7d2e062057f016389
ed13santi <37627980+ed13santi@users.noreply.github.com>
2022-07-11 15:50:46 +0100

    fixed dictionary functions

--- trythings.py
+++ trythings.py
@@ -7,15 +7,14 @@ def init_nested_dict(keys, val):
     return tmp
 
 def modify_nested_dict(d, keys, val, i):
-    tmp = init_nested_dict(keys[i:], val) #initialise Q value at 0
-    for j in range(i+1, 0, -1):
-        tmp_dict = d
-        for k in keys[:-1]:
-            tmp_dict = tmp_dict[k]
-        tmp_dict[i] = tmp
-        tmp = tmp_dict
-    
-    return tmp
+    tmp = init_nested_dict(keys[i:], val) 
+    tmp_dict = d
+    for k in keys[:i]:
+        if k not in tmp_dict:
+            tmp_dict[k] = {}
+        tmp_dict = tmp_dict[k]
+    tmp_dict[keys[i]] = tmp[keys[i]]
+    return d
 
 d = {}
 keys = [0]
@@ -25,6 +24,14 @@ i = 0
 out = modify_nested_dict({}, [0], {}, 0)
 print(out)
 
-
 out = modify_nested_dict(out, [5], {}, 0)
+print(out)
+
+out = modify_nested_dict(out, [5, 3], {}, 1)
+print(out)
+
+out = modify_nested_dict(out, [0, 3], {}, 1)
+print(out)
+
+out = modify_nested_dict(out, [2, 3], {}, 1)
 print(out)
